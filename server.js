41d652f7db23db983a8879ddb0903eb12f21250d
Laura Gómez Herrera <lauragh25@gmail.com>
2022-05-18 00:04:48 +0200

    name commit

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
-const data = require("./db.js");
+// server.js
+const jsonServer = require('json-server') //1
+const server = jsonServer.create()//2
+const router = jsonServer.router('db.json')//3
+router.db._.id = 'employeeid';//3a
+const middlewares = jsonServer.defaults()//4 
+server.use(middlewares) //5
 
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router(data);
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+// To handle POST, PUT and PATCH 
+// you need to use a body-parser. Using JSON Server's bodyParser
+// server.use(jsonServer.bodyParser);
 
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
+//6 Redirect URL's- 
+// Have all URLS with customurl redirected 
+// Have all URLS prefixed with a /api
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+'/customurl/*': '/$1'
+}))
+//7 Ensure every POST has a createdAt date
+/*server.use((req, res, next) => {
+  if (req.method === "POST") {
+    req.body.createdAt = Date.now();
+  }
+  // Continue to JSON Server router
+  next();
+});
+//8 Ensure every POST of an employee, it has lname for sure. Otherwise return 400 and a message
+server.post("/employees/", function(req, res, next) {
+  const error = validateRequest(req.body);
+  if (error) {
+    res.status(400).send(error);
+  } else {                    
+    next();
+  }
+});*/
+server.use(router) 
+server.listen(3000, () => {//10 using 3000 port
+  console.log('JSON Server is running')
+})
+function validateRequest(employee){
+//9 validate function used by POST above
+    if (!employee.lname) return "lname is required.";
+}
